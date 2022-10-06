25b9cf64c54d62141795238310e7735af588170e
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2022-10-06 10:38:16 +0500

    update computers data

--- data/computers.py
+++ data/computers.py
@@ -2,33 +2,38 @@ programming_about = '''
 Dasturlash uchun komputerning minimal talablari
 Tezkor xotira: 8 GB
 Doimit xotira: 256 GB SSD
-Protsessor: Intel core i5, AMD Ryzen 5
-Monitor o'chami: 1024×768
+Protsessor: Intel core i3, AMD Ryzen 3
+Brend: HP, Dell, MSI, Apple Macbook Pro
 '''
 
 graphics_about = '''
-Grafik dizayn uchun komputerning minimal talablari
+Grafik dizayn uchun tavsiya qilinadigan kompyuter
 Tezkor xotira: 12 GB
 Doimit xotira: 512 GB SSD
 Protsessor: Intel core i5, AMD Ryzen 5
 Grafik karta: NVIDIA GTX 1650
-Monitor o'chami: 1920×1080
+Monitor o'chami: 1920×1080 Full HD
+Brend: Apple Macbook, Microsoft Surface, HP
 '''
 
 gaming_about = '''
-Requirement	Minimum
-RAM	2 GB of free RAM
-CPU	Any modern CPU
-Disk space	2.5 GB and another 1 GB for caches
-Monitor resolution	1024×768
+O'yin uchun tavsiya qilanadigan kompyuter
+Tezkor xotira: 16 GB
+Doimit xotira: 512 GB SSD
+Protsessor: Intel core i7, AMD Ryzen 5
+Grafik karta: NVIDIA RTX 3050
+Monitor: FULL HD, 2K
+Brend: Asus, Lenove, HP Pavilion
 '''
 
 office_about = '''
-Requirement	Minimum
-RAM	2 GB of free RAM
-CPU	Any modern CPU
-Disk space	2.5 GB and another 1 GB for caches
-Monitor resolution	1024×768
+Office uchun tavsiya qilanadigan kompyuter
+Tezkor xotira: 4 GB
+Doimit xotira: 400 GB HDD
+Protsessor: Har qanday zamonaviy protsessor
+Grafik karta: NVIDIA RTX 3050
+Monitor: HD, Full HD
+Brend: Asus, Lenove, Acer, Apple Macbook Air
 '''
 
 computer_types = {
@@ -36,34 +41,35 @@ computer_types = {
         'title': 'Dasturlash uchun kompyuterlar',
         'about': programming_about,
         'computers': [
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
+            'https://t.me/enter_computers/895',
+            'https://t.me/enter_computers/894',
+            'https://t.me/enter_computers/868',
         ]
     },
     'graphics': {
         'title': 'Grafik dizayn uchun kompyuterlar',
         'about': graphics_about,
         'computers': [
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
+            'https://t.me/enter_computers/886',
+            'https://t.me/enter_computers/689',
         ]
     },
     'gaming': {
         'title': 'O\'yin uchun kompyuterlar',
         'about': gaming_about,
         'computers': [
-            'https://t.me/sanjarbek_saminjonov/560',
-            'https://t.me/sanjarbek_saminjonov/560',
+            'https://t.me/enter_computers/893',
+            'https://t.me/enter_computers/887',
+            'https://t.me/enter_computers/872',
         ]
     },
     'office': {
         'title': 'Ofis uchun kompyuter',
         'about': office_about,
         'computers': [
-            'https://t.me/sanjarbek_saminjonov/560',
+            'https://t.me/enter_computers/882',
+            'https://t.me/enter_computers/889',
+            'https://t.me/enter_computers/888',
         ]
     }
 }
